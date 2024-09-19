0b7c2f4d5352bab095ee5f2d81ade3ef69fd6c58
Tilen Krivec <tilen.krivec@gmail.com>
2024-09-20 00:08:41 +0200

    db2

--- db/db.ts
+++ db/db.ts
@@ -1,11 +1,15 @@
-import { config } from 'dotenv';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import postgres from 'postgres';
-import { profilesTable } from './schema/profiles-schema';
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { profilesTable } from "./schema/profiles-schema";
 
-config({ path: '.env.local' });
+config({ path: ".env.local" });
 
-const schema = { profiles: profilesTable };
+const schema = {
+  profiles: profilesTable,
+  
+};
 
 const client = postgres(process.env.DATABASE_URL!);
+
 export const db = drizzle(client, { schema });
